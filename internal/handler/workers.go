df139b6fcf1499235f4262deb0e9148621418dd6
hardvlad <hardvlad@gmail.com>
2025-12-31 18:36:58 +0500

    Added http request retry - fix

--- internal/handler/workers.go
+++ internal/handler/workers.go
@@ -54,7 +54,7 @@ func fetchOrderAccruals(data Handlers, url string) (string, float64, error) {
 
 	for {
 		data.Logger.Infow("Getting accruals", "url", url)
-		response, err := retry.Retry(3, 2*time.Second, func() (*http.Response, error) { return http.Get(url) })
+		response, err := retry.Retry(3, 2, func() (*http.Response, error) { return http.Get(url) })
 		if err != nil {
 			data.Logger.Debugw(err.Error(), "event", "fetchOrderAccruals - http.Get error", "url", url)
 			return "", 0, err
