5a1ecee66ff71a2c995180373b6bfe4da32d540d
hardvlad <hardvlad@gmail.com>
2025-12-31 17:20:00 +0500

    Fixes - добавил обработку 429

---
+++ internal/handler/workers.go
@@ -0,0 +1,98 @@
+package handler
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/url"
+	"strconv"
+	"sync"
+	"time"
+)
+
+func CreateWorkers(numWorkers int, data Handlers, ch chan string, wg *sync.WaitGroup) {
+	for i := 0; i < numWorkers; i++ {
+		wg.Add(1)
+		go accrualsWorker(i, data, ch, wg)
+	}
+}
+
+// accrualsWorker воркер, слушающий канал, в который поступают номера заказов для обработки
+func accrualsWorker(id int, data Handlers, ch chan string, wg *sync.WaitGroup) {
+	for orderNumber := range ch {
+		err := processOrderAccruals(data, orderNumber)
+		if err != nil {
+			data.Logger.Errorw("accrualsWorker: processOrderAccruals error", "id", id, "orderNumber", orderNumber, "error", err)
+		}
+	}
+	wg.Done()
+}
+
+// processOrderAccruals функция получения статуса заказа и начислений бонусов из внешнего сервиса
+func processOrderAccruals(data Handlers, number string) error {
+	CheckAndPause()
+	_ = data.Store.SetOrderStatusAccrual(number, "PROCESSING", 0)
+
+	accrualURL, err := url.JoinPath(data.Conf.AccrualAddress, "/api/orders/", number)
+	if err != nil {
+		return err
+	}
+
+	status, accrual, err := fetchOrderAccruals(data, accrualURL)
+	if err != nil {
+		return err
+	}
+
+	return data.Store.SetOrderStatusAccrual(number, status, accrual)
+}
+
+// fetchOrderAccruals функция, в которой происходит обращение к внешнему сервису начислений
+// и ожидающая окончание начислений, периодически запрашивая внешний сервис
+func fetchOrderAccruals(data Handlers, url string) (string, float64, error) {
+	var status string
+	var accrual float64
+
+	for {
+		data.Logger.Infow("Getting accruals", "url", url)
+		response, err := http.Get(url)
+		if err != nil {
+			data.Logger.Debugw(err.Error(), "event", "fetchOrderAccruals - http.Get error", "url", url)
+			return "", 0, err
+		}
+		defer response.Body.Close()
+
+		if response.StatusCode == http.StatusTooManyRequests {
+			handle429(response, data)
+			waitTime := response.Header.Get("Retry-After")
+			data.Logger.Debugw("fetchOrderAccruals - received 429 Too Many Requests", "url", url, "waitTime", waitTime)
+			waitSeconds, err := strconv.Atoi(waitTime)
+			if err != nil {
+				waitSeconds = 1
+			}
+
+			time.Sleep(time.Second * time.Duration(waitSeconds))
+			continue
+		}
+
+		if response.StatusCode == http.StatusNoContent {
+			status = "NEW"
+			accrual = 0
+			break
+		}
+
+		if response.StatusCode == http.StatusOK {
+			var resp AccrualResponse
+			dec := json.NewDecoder(response.Body)
+			if err := dec.Decode(&resp); err != nil {
+				time.Sleep(time.Millisecond * 100)
+				continue
+			}
+
+			if resp.Status == "INVALID" || resp.Status == "PROCESSED" {
+				accrual = resp.Accrual
+				status = resp.Status
+				break
+			}
+		}
+	}
+	return status, accrual, nil
+}
---
+++ internal/handler/workesLock.go
@@ -0,0 +1,54 @@
+package handler
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+	"sync"
+	"time"
+)
+
+var (
+	isRateLimitedMux sync.Mutex
+	isRateLimited    bool
+	resumeTime       time.Time
+)
+
+func handle429(resp *http.Response, data Handlers) time.Duration {
+	if resp.StatusCode == http.StatusTooManyRequests {
+		isRateLimitedMux.Lock()
+		defer isRateLimitedMux.Unlock()
+
+		var waitDuration time.Duration
+		if retryAfterStr := resp.Header.Get("Retry-After"); retryAfterStr != "" {
+			if seconds, err := strconv.Atoi(retryAfterStr); err == nil {
+				waitDuration = time.Duration(seconds) * time.Second
+			} else {
+				waitDuration = 5 * time.Second
+			}
+		} else {
+			waitDuration = 5 * time.Second
+		}
+
+		resumeTime = time.Now().Add(waitDuration)
+		isRateLimited = true
+		data.Logger.Debugw("Status 429", "event", "Получен статус 429, засыпаем на", "seconds", waitDuration)
+		return waitDuration
+	}
+	return 0
+}
+
+func CheckAndPause() {
+	isRateLimitedMux.Lock()
+	if isRateLimited {
+		waitTime := time.Until(resumeTime)
+		if waitTime > 0 {
+			isRateLimitedMux.Unlock()
+			fmt.Printf("Still rate limited. Waiting for %v...\n", waitTime)
+			time.Sleep(waitTime)
+			isRateLimitedMux.Lock()
+		}
+		isRateLimited = false
+	}
+	isRateLimitedMux.Unlock()
+}
